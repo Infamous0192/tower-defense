3fe143959a77530830e6efd3b82f5b5913ec8ded
Dwa Meizadewa <infamous0192@gmail.com>
2021-09-21 21:10:36 +0800

    Add readme

---
+++ Assets/Scripts/TowerPlacement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TowerPlacement: MonoBehaviour {
+  private Tower _placedTower;
+
+  private void OnTriggerEnter2D(Collider2D collision) {
+    if (_placedTower != null) return;
+
+    Tower tower = collision.GetComponent<Tower>();
+
+    if (tower != null) {
+      tower.SetPlacePosition(transform.position);
+
+      _placedTower = tower;
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D collision) {
+    if (_placedTower == null) return;
+
+    _placedTower.SetPlacePosition(null);
+
+    _placedTower = null;
+  }
+}
